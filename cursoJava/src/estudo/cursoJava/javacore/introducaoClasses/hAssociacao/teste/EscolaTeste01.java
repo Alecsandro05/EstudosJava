3dcf80df578f141aede146642bfced3a7a0a23be
Alecsandro05 <alecsandrosales1@gmail.com>
2024-12-20 18:01:57 -0300

    Estudo sobre Associação unidirecional de muitos para um

---
+++ cursoJava/src/estudo/cursoJava/javacore/introducaoClasses/hAssociacao/teste/EscolaTeste01.java
@@ -0,0 +1,15 @@
+package estudo.cursoJava.javacore.introducaoClasses.hAssociacao.teste;
+
+import estudo.cursoJava.javacore.introducaoClasses.hAssociacao.dominio.Escola;
+import estudo.cursoJava.javacore.introducaoClasses.hAssociacao.dominio.Professor;
+
+public class EscolaTeste01 {
+    public static void main(String[] args) {
+        Professor professor1 = new Professor("Jiraya Sensei");
+        Professor professor2 = new Professor("Kakashi Sensei");
+        Professor[] professores = {professor1,professor2};
+        Escola escola = new Escola("Konoha",professores);
+
+escola.imprime();
+    }
+}
