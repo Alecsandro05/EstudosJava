26721f6c0eba4df57d80306eb5115f973e9c0a49
Alecsandro05 <alecsandrosales1@gmail.com>
2025-01-08 18:02:09 -0300

    apenas teste o pacote Scanner

--- cursoJava/src/estudo/cursoJava/introducao/Lendo_dados_teclado.java
+++ cursoJava/src/estudo/cursoJava/introducao/Lendo_dados_teclado.java
@@ -1,22 +1,20 @@
 package estudo.cursoJava.introducao;
 
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Lendo_dados_teclado {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-//        System.out.println("Digite seu nome completo:");
-//        String nomeCompleto = scan.nextLine();
-//        System.out.println("Seu nome completo é " + nomeCompleto);
-//
-//        System.out.println("Digite seu primeiro nome :");
-//        String primeiroCompleto = scan.next();
-//        System.out.println("Seu primeiro nome é " + primeiroCompleto);
-
-        System.out.println("Digite a sua idade:");
-        int idade = scan.nextInt();
-        System.out.println("Sua idade é: " + idade);
+        System.out.println("O grande software de previsão do futuro");
+        System.out.println("Digite sua pergunta e eu responderei sim ou não");
+        String question = scan.nextLine();
+        if(question.charAt(0) == ' '){
+            System.out.println("sim");
+        }else {
+            System.out.println("Não");
+        }
 
     }
 }
