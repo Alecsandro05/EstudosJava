5d5ead6119713a7cac7f047d25c9e1d272842876
Alecsandro05 <alecsandrosales1@gmail.com>
2025-01-22 18:09:14 -0300

    Exercicio sobre associacao

---
+++ cursoJava/src/estudo/cursoJava/javacore/introducaoClasses/zExercicioAssociacao/AssociacaoTeste.java
@@ -0,0 +1,17 @@
+package estudo.cursoJava.javacore.introducaoClasses.zExercicioAssociacao;
+
+public class AssociacaoTeste {
+    public static void main(String[] args) {
+        Local local = new Local("Brasilia");
+        Aluno aluno = new Aluno("Alecsandro Sales", 19);
+        Professor professor = new Professor("Jorge", "Back-end");
+        Aluno[] alunosParaSeminario = {};
+
+        Seminario seminario = new Seminario("Programação orientada a objetos",alunosParaSeminario, local);
+
+        Seminario[] seminariosDisponiveis = {seminario};
+
+        professor.setSeminarios(seminariosDisponiveis);
+        professor.imprime();
+    }
+}
