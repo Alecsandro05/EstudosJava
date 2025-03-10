0abca8a5ba144b50a2fcbd75b277afd31550e688
Alecsandro05 <alecsandrosales1@gmail.com>
2025-03-10 17:35:31 -0300

    study: metod toString

---
+++ cursoJava/src/estudo/cursoJava/javacore/introducaoPoo/JsobreEscrita/teste/AnimeTeste01.java
@@ -0,0 +1,10 @@
+package estudo.cursoJava.javacore.introducaoPoo.JsobreEscrita.teste;
+
+import estudo.cursoJava.javacore.introducaoPoo.JsobreEscrita.dominio.Anime;
+
+public class AnimeTeste01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Naruto");
+        System.out.println(anime.toString());
+    }
+}
